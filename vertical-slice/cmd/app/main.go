4c7febb50c1fa646e72f65517877e251c7670584
abdorrahamni <mohammad02abdorrahmani@gmail.com>
2025-01-22 10:29:18 +0330

    ✨ feat(vertical-slice): update server to use config for port

--- vertical-slice/cmd/app/main.go
+++ vertical-slice/cmd/app/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"vertical-slice/config"
 )
 
 func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Could not load config: %v", err)
+	}
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	err = r.Run(":" + cfg.App.Port)
+	if err != nil {
+		log.Fatalf("Faild to start server : %v", err)
+	}
 }
