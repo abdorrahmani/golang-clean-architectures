700cfc601bcd498c0e3a02c1f7342ac3cfc9c7b7
abdorrahamni <mohammad02abdorrahmani@gmail.com>
2025-01-25 16:56:19 +0330

    🐛 fix(product): change id type to uint

--- vertical-slice/internal/modules/products/models/product.go
+++ vertical-slice/internal/modules/products/models/product.go
@@ -1,7 +1,7 @@
 package models
 
 type Product struct {
-	ID          int     `json:"id" gorm:"primaryKey;autoIncrement"`
+	ID          uint    `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name        string  `json:"name" gorm:"size:255;not null"`
 	Description string  `json:"description" gorm:"type:text"`
 	Price       float64 `json:"price" gorm:"type:decimal(10,2);not null"`
