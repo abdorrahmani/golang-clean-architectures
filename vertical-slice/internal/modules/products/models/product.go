4b29f60e0bab6ddc85e450cd128936dddb60e501
abdorrahamni <mohammad02abdorrahmani@gmail.com>
2025-01-26 09:47:13 +0330

    🐛 fix(product): change timestamp type to time

--- vertical-slice/internal/modules/products/models/product.go
+++ vertical-slice/internal/modules/products/models/product.go
@@ -1,10 +1,12 @@
 package models
 
+import "time"
+
 type Product struct {
-	ID          uint    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name        string  `json:"name" gorm:"size:255;not null"`
-	Description string  `json:"description" gorm:"type:text"`
-	Price       float64 `json:"price" gorm:"type:decimal(10,2);not null"`
-	CreatedAt   string  `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   string  `json:"updated_at" gorm:"autoUpdateTime"`
+	ID          uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name        string    `json:"name" gorm:"size:255;not null"`
+	Description string    `json:"description" gorm:"type:text"`
+	Price       float64   `json:"price" gorm:"type:decimal(10,2);not null"`
+	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
