2323908e8a0e85af3e177d0e64127adc66ff760a
abdorrahmani <mohammad02abdorrahmani@gmail.com>
2025-07-26 15:16:05 +0330

    ✨ feat(server): integrate Gin framework and add basic ping endpoint

--- hexagonal/cmd/app/main.go
+++ hexagonal/cmd/app/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"hexagonal/internal/config"
+	"log"
+	"net/http"
+)
+
 func main() {
-	// TODO: Implement THE HEXAGONAL ARCHITECTURE
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err = r.Run(":", cfg.App.Port)
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
